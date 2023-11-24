a9e89bc129bf489a82498422a3de5b673450dc83
Özge Ünal <oezgeunal@gmail.com>
2023-11-24 16:01:49 +0300

    “Özellik 1, testleri ile tamamlandı”

--- 210401117/Vize_Q1_tests_210401117_Ozge_Unal.R
+++ 210401117/Vize_Q1_tests_210401117_Ozge_Unal.R
@@ -3,3 +3,7 @@ library(testthat)
 testthat::test_that("Global Workspace'te spotify_token adlı bir değişken olmalı" , {
   expect_true(exists("spotify_token", envir = .GlobalEnv))
 })
+
+testthat::test_that("spotify_token adlı değişkenin tipi 'function' olmalı", {
+  expect_true(is.function(spotify_token))
+})
